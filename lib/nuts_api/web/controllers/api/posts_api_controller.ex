ef2156e099647e366d11d8cec4241a9c069f2e40
Smigus <Smigum@outlook.com>
2017-08-31 14:51:38 -0500

    Rewrote the send connection code in controller actions

--- lib/nuts_api/web/controllers/api/posts_api_controller.ex
+++ lib/nuts_api/web/controllers/api/posts_api_controller.ex
@@ -15,24 +15,22 @@ defmodule NutsApi.Web.PostApiController do
 
   def like(conn, %{"id" => id}) do
     Content.like_post(id)
-    conn
-    |> put_status(200)
+    send_resp conn, 200, ""
   end
 
   def unlike(conn, %{"id" => id}) do
     Content.unlike_post(id)
-    conn
-    |> put_status(200)
+    send_resp conn, 200, ""
   end
 
   def add_comment(conn, %{"id" => post_id, "text" => text}) do
     post = Content.get_post!(post_id)
     text = String.replace(text, "_", " ")
     with {:ok, comment} <- Content.create_comment_for_post(post, %{text: text}) do
-      json conn, %{status_code: 200}
+      send_resp conn, 200, ""
     else
       {:error, reason} ->
-        json conn, %{status_code: 400, message: "Request body error"}
+        send_resp conn, 400, "Request body error"
     end
   end
 
@@ -40,10 +38,10 @@ defmodule NutsApi.Web.PostApiController do
     with {:ok, encoded_body, _} <- Plug.Conn.read_body(conn),
       {:ok, body} <- Poison.decode(encoded_body),
       {:ok, post} <- create_post_from_request_body(body) do
-      json conn, %{status_code: 200}
+      send_resp conn, 200, ""
     else
       {:error, reason} ->
-        json conn, %{status_code: 400, message: "Request body error"}
+        send_resp conn, 400, "Request body error"
     end
   end
 
